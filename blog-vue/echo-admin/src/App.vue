9bba2d54f843b93d182c4f9c71d5daca6320cf88
scottzwells <2228630475@qq.com>
2024-07-02 14:47:08 +0800

    重置后台管理

---
+++ blog-vue/echo-admin/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+console.log("helloworld")
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+
+}
+body, html {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ blog-vue/echo-admin/src/views/Layout.vue
@@ -0,0 +1,156 @@
+<script setup>
+import {
+    Management,
+    Promotion,
+    UserFilled,
+    User,
+    Crop,
+    EditPen,
+    SwitchButton,
+    CaretBottom
+} from '@element-plus/icons-vue'
+import avatar from '@/assets/default.png'
+
+import {useRouter} from 'vue-router'
+let router = useRouter()
+const exit = ()=>{
+    router.push('/login')
+}
+</script>
+
+<template>
+    <!-- elemnt-plus中的容器 -->
+    <el-container class="layout-container">
+        <!-- 左侧菜单 -->
+        <el-aside width="200px">
+            <div class="el-aside__logo">
+                <h2>InsightEcho</h2>
+            </div>
+            <!-- 菜单标签 -->
+            <el-menu active-text-color="#ffd04b" background-color="#232323"  text-color="#fff"
+                router>
+                <el-menu-item index="/article/category">
+                    <el-icon>
+                        <Management />
+                    </el-icon>
+                    <span>文章分类</span>
+                </el-menu-item>
+                <el-menu-item index="/article/manage">
+                    <el-icon>
+                        <Promotion />
+                    </el-icon>
+                    <span>文章管理</span>
+                </el-menu-item>
+                <el-sub-menu >
+                    <template #title>
+                        <el-icon>
+                            <UserFilled />
+                        </el-icon>
+                        <span>个人中心</span>
+                    </template>
+                    <el-menu-item index="/user/info">
+                        <el-icon>
+                            <User />
+                        </el-icon>
+                        <span>基本资料</span>
+                    </el-menu-item index="/user/avatar">
+                    <el-menu-item >
+                        <el-icon>
+                            <Crop />
+                        </el-icon>
+                        <span>更换头像</span>
+                    </el-menu-item>
+                    <el-menu-item index="/user/password">
+                        <el-icon>
+                            <EditPen />
+                        </el-icon>
+                        <span>重置密码</span>
+                    </el-menu-item>
+                </el-sub-menu>
+            </el-menu>
+        </el-aside>
+        <!-- 右侧主区域 -->
+        <el-container>
+            <!-- 头部区域 -->
+            <el-header>
+                <div></div>
+                <!-- 下拉菜单 -->
+                <el-dropdown placement="bottom-end">
+                    <span class="el-dropdown__box">
+                        <el-avatar :src="avatar" />
+                        <el-icon>
+                            <CaretBottom />
+                        </el-icon>
+                    </span>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item command="profile" :icon="User">基本资料</el-dropdown-item>
+                            <el-dropdown-item command="avatar" :icon="Crop">更换头像</el-dropdown-item>
+                            <el-dropdown-item command="password" :icon="EditPen">重置密码</el-dropdown-item>
+                            <el-dropdown-item command="logout" :icon="SwitchButton" @click="exit">退出登录</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </el-header>
+            <!-- 中间区域 -->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+            <!-- 底部区域 -->
+            <el-footer>InsightEcho ©2024 Created by </el-footer>
+        </el-container>
+    </el-container>
+</template>
+
+<style lang="scss" scoped>
+.layout-container {
+    height: 100vh;
+
+    .el-aside {
+        background-color: #232323;
+
+        &__logo {
+            height: 100px;
+            line-height: 100px;
+            color:#fff;
+            text-align: center;
+            cursor:default;
+        }
+
+        .el-menu {
+            border-right: none;
+        }
+    }
+
+    .el-header {
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+
+        .el-dropdown__box {
+            display: flex;
+            align-items: center;
+
+            .el-icon {
+                color: #999;
+                margin-left: 10px;
+            }
+
+            &:active,
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+
+    .el-footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 14px;
+        color: #666;
+    }
+}
+</style>
