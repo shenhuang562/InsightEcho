7717f34e6ce542e2fdda4ad4366c15ce30d1de80
草 <1833674465@qq.com>
2024-07-03 15:41:59 +0800

    测试Mapper功能

---
+++ blog-springboot/src/src/test/java/com/InsightEcho/mapper/BlogFileMapperTest.java
@@ -0,0 +1,37 @@
+package com.InsightEcho.mapper;
+
+import com.InsightEcho.entity.BlogFile;
+import com.InsightEcho.model.vo.query.FileQuery;
+import com.InsightEcho.model.vo.response.FileResp;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BlogFileMapperTest {
+    @Autowired
+    private BlogFileMapper blogFileMapper;
+
+    BlogFile blogFile = BlogFile.builder()
+            .fileUrl("test")
+            .fileName("test")
+            .fileSize(1024)
+            .extendName("jpg")
+            .filePath("test")
+            .isDir(1)
+            .build();
+
+    final FileQuery fileQuery = new FileQuery();
+
+    @Test
+    void selectFileVOList() {
+        blogFileMapper.insert(blogFile);
+        fileQuery.setFilePath("test");
+        List<FileResp> list = blogFileMapper.selectFileVOList(fileQuery);
+        System.out.println(list);
+    }
+}
